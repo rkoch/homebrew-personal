f80d10a2e06408ec9fae7160befd493c1ff211d7
Remo Koch <remkoch@gmail.com>
2017-09-13 18:36:28 +0200

    Add NautilusLifeline.app

---
+++ Casks/nautilus-lifeline.rb
@@ -0,0 +1,10 @@
+cask 'nautilus-lifeline' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://updates.nautiluslifeline.com/Lifeline-Setup.dmg'
+  name 'Nautilus Lifeline'
+  homepage 'http://www.nautiluslifeline.com/'
+
+  app 'NautilusLifeline.app'
+end
