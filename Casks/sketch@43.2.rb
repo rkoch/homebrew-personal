21b4a640e326c8175892e824d4a600942193480b
Remo Koch <remkoch@gmail.com>
2018-01-14 17:42:59 +0100

    Add Sketch.app 43.2

---
+++ Casks/sketch@43.2.rb
@@ -0,0 +1,25 @@
+cask 'sketch@43.2' do
+  version '43.2-39069'
+  sha256 '4a67e22b0f0794f8ccc2be169236b7d5baf0f8a02082dcb3d4e2a41fd2ad4c13'
+
+  url "https://download.sketchapp.com/sketch-#{version}.zip"
+  appcast 'https://download.sketchapp.com/sketch-versions.xml',
+          checkpoint: 'f57fa2402d9a3657b82bfe49249867b110c8304a7eae0800a7f6d03e8c647ce7'
+  name 'Sketch'
+  homepage 'https://www.sketchapp.com/'
+
+  auto_updates true
+
+  app 'Sketch.app'
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.sfl*',
+               '~/Library/Application Support/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.bohemiancoding.sketch3',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3',
+               '~/Library/Logs/com.bohemiancoding.sketch3',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.LSSharedFileList.plist',
+               '~/Library/Preferences/com.bohemiancoding.sketch3.plist',
+               '~/Library/Cookies/com.bohemiancoding.sketch3.binarycookies',
+             ]
+end
